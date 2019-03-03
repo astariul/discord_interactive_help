c676eb8c7ca51a7c76732baaa94273bf2473e418
Astariul <remondnicola@gmail.com>
2019-03-03 21:05:03 +0900

    pypi name updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = 'discord-interactive',         # How you named your package folder (MyLib)
-  packages = ['discord-interactive'],   # Chose the same as "name"
+  packages = ['discord_interactive'],   # Chose the same as "name"
   version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A package allowing you to display interactive help in Discord easily',   # Give a short description about your library
