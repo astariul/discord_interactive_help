f1b1d5c5c8304691655c0067d8c3da2e5b9b75d0
Astariul <remondnicola@gmail.com>
2019-05-19 18:51:34 +0900

    Rewrite finish

--- discord_interactive/link.py
+++ discord_interactive/link.py
@@ -21,7 +21,7 @@ class Link:
         path (int): Which child page will be displayed. Default to 0 (the first
             Page of the list).
     """
-    def __init__(self, pages, description="", callbacks=[]):
+    def __init__(self, pages, description=None, callbacks=[]):
         """ Link constructor
 
         Create a Link with a list of child pages, a description, and 
@@ -30,20 +30,33 @@ class Link:
         Args:
             pages (list of Page or Page): List of pages associated to this link.
             description (str, optional): Description of this link, to explain to 
-                user the effect of this link.
+                user the effect of this link. Defaults to `None`.
             callbacks (list, optional): List of functions to call when taking 
                 this link. Defaults to empty list.
         """
+        # Normalize input
         if type(pages) == list:
             self.pages = pages
         else:
             self.pages = [pages]
+        if type(callbacks) == list:
+            self.callbacks = callbacks
+        else:
+            self.callbacks = [callbacks]
+
         self.description = description
-        self.callbacks = callbacks
         self.path = 0
 
-    # TODO def get_linked_page()
-    # TODO def get_description()
+    def page(self):
+        """
+        This method is called by the Help, after calling the callbacks.
+        So the path was updated (or not) to select the right page, and we should 
+        return the appropriate page.
+
+        Returns:
+            Page: The page selected by callbacks (or default choice).
+        """
+        return self.pages[self.path]
 
 class ReactLink(Link):
     """ Class for link using reaction to naviguate to next page.
@@ -51,7 +64,7 @@ class ReactLink(Link):
     Attributes:
         reaction (str): Reaction needed by this link to display the page.
     """
-    def __init__(self, reaction, pages, description="", callbacks=[]):
+    def __init__(self, reaction, pages, description=None, callbacks=[]):
         """ ReactLink constructor
 
         Create a ReactLink to other pages, with a reaction, and a possibly a 
@@ -61,19 +74,17 @@ class ReactLink(Link):
             reaction (str): Reaction needed by this link to display the page.
             pages (list of Page or Page): Page to display when this link is used.
             description (str, optional): Description of this link, to explain to 
-                user the effect of this link.
+                user the effect of this link. Defaults to `None`.
             callbacks (list, optional): List of functions to call when taking 
                 this link. Defaults to empty list.
         """
         super(ReactLink, self).__init__(pages, description, callbacks)
         self.reaction = reaction
 
-    # TODO def get_reaction()
-
 class MsgLink(Link):
     """ Class for link using message to naviguate to next page.
     """
-    def __init__(self, pages, description="", callbacks=[]):
+    def __init__(self, pages, description=None, callbacks=[]):
         """ MsgLink constructor
 
         Create a MsgLink with a list of child pages, a description, and 
@@ -82,7 +93,7 @@ class MsgLink(Link):
         Args:
             pages (list of Page or Page): List of pages associated to this link.
             description (str, optional): Description of this link, to explain to 
-                user the effect of this link.
+                user the effect of this link. Defaults to `None`.
             callbacks (list, optional): List of functions to call when taking 
                 this link. Defaults to empty list.
         """
@@ -102,4 +113,4 @@ class RootLink(Link):
             callbacks (list, optional): List of functions to call when taking 
                 this link. Defaults to empty list.
         """
-        super(RootLink, self).__init__(pages, callbacks)
+        super(RootLink, self).__init__(pages, callbacks=callbacks)
