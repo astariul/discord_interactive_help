e4b5224d5962371ff5407c80c082d3e314f9e798
Astariul <remondnicola@gmail.com>
2019-05-25 22:15:56 +0900

    package pushed to pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'discord_interactive',         # How you named your package folder (MyLib)
   packages = ['discord_interactive'],   # Chose the same as "name"
-  version = '2.1',      # Start with a small number and increase it with every change you make
+  version = '3.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A package allowing you to display interactive help in Discord easily',   # Give a short description about your library
   long_description='A package to help you build an interactive help for your Discord bot.\n\nPlease refer to the github page for more information : https://github.com/astariul/discord_interactive_help/tree/master\n\nYou can also refer to the wiki for detailed explanations : https://github.com/astariul/discord_interactive_help/wiki',
@@ -11,7 +11,7 @@ setup(
   author = 'Nicolas REMOND',                   # Type in your name
   author_email = 'remondnicola@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/astariul/discord_interactive_help',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/astariul/discord_interactive_help/archive/v2.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/astariul/discord_interactive_help/archive/v3.tar.gz',    # I explain this later on
   keywords = ['Discord', 'Interactive', 'Help'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
